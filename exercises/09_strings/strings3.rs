86b27a8c67013126539fbb6d613d302316af8756
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-06-12 18:08:50 +0500

    quiz2 is unsolved

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,23 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+   input.trim().to_string() 
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+   let hello_world =input.to_owned()+" world!";
+   hello_world
 }
+
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let replaced_string = input.to_owned().replace("cars","balloons");
+    return replaced_string;
 }
 
 #[cfg(test)]
